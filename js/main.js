2047e7e73b63d3d89817dd3050d36e6358187063
tonybwatson <tonybwatson@gmail.com>
2021-09-10 16:41:05 -0400

    change sound, appearance tweaks, move alarm call from html to js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,14 @@ function updateTime() {
     var minute = date.getMinutes();
     var second = date.getSeconds();
 
-    // if hour, minute or second are less than 10, add a zero in front
+    // convert to standard time
+    if (hour > 12) {
+        hour = (hour - 12);
+    } else if (hour == 0) {
+        hour = '12';
+    }
+
+    // if hour, minute or second are less than 10, add a zero in front to format
     if (hour < 10) {
         hour = '0' + hour;
     }
@@ -21,14 +28,32 @@ function updateTime() {
 
     // variable to format time as a clock would, with colons between hour/minute/second
     var currentTime = `${hour}<span class="blink">:</span>${minute}<span class="blink">:</span>${second}`;
-    // actually display time in 'time' in HTML
+
+    // display time in 'time' in HTML
     document.getElementById('time').innerHTML=currentTime
 }
 
-// update updateTime every second
+// update updateTime every second, continuous
 setInterval(updateTime, 1000)
 
+// function for alarm - play sound, show alert when executed
 function alarm() {
     document.getElementById('alertSound').play();
     alert('!');
 }
+
+// variables to run different alarms
+var eightHour = document.getElementById('eightHour');
+var tenMinute = document.getElementById('tenMinute');
+var tenSecond = document.getElementById('tenSecond');
+
+// function calls for alarms
+eightHour.onclick = setTimeout(alarm, 28800000);
+tenMinute.onclick = setTimeout(alarm, 60000);
+tenSecond.onclick = setTimeout(alarm, 10000);
+
+
+// specific time of day alarm goes here
+// if (hour === 15 && minute === 30 && second === 0) {
+//     alarm();
+// }
